3c6fa896328555b5c831ef29646b0921c64de8cb
Adhikansh Mittal <adhikanshmittal@gmail.com>
2019-05-25 13:28:59 +0530

    Patterns for dealing with Asynchronous code

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 console.log('Before');
-
-setTimeout(()=>{
-    console.log('Reading a user from a database...');
-},2000);
-
+const user = getUser(1);
+console.log(user);
 console.log('After');
 
+// Ways to get result of Async Opperation
+// 1.Callbacks
+// 2.Promises
+// 3.Async/await
+function getUser(id){
+    setTimeout(()=>{
+        console.log('Reading a user from a database...');
+        return{id:id,gitHubUsername:'HrithikMittal'}
+    },2000); 
+
+    return 1;
+}
